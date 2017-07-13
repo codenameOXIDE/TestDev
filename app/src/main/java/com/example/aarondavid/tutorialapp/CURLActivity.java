77455c383fe9e103d111adb8c5455f91f1e5b80d
codenameOXIDE <aaron.david91@gmail.com>
2017-07-13 13:39:51 +0800

    Made a CMS API Call, Display next

--- app/src/main/java/com/example/aarondavid/tutorialapp/CURLActivity.java
+++ app/src/main/java/com/example/aarondavid/tutorialapp/CURLActivity.java
@@ -1,15 +1,37 @@
 package com.example.aarondavid.tutorialapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 public class CURLActivity extends AppCompatActivity
 {
@@ -30,7 +52,7 @@ public class CURLActivity extends AppCompatActivity
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
 
-        String url = "https://www.google.com";
+        String url = "https://dumc.ccbchurch.com/api.php?srv=individual_profile_from_id&individual_id=9";
 
         //Request a string from the provided url
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>()
@@ -38,7 +60,18 @@ public class CURLActivity extends AppCompatActivity
             @Override
             public void onResponse(String response)
             {
-                mTextView.setText("Response is: " + response.substring(0, 500));
+                //mTextView.setText("Response is: " + response.substring(0, 6000));
+                try {
+                    convertStringToDoc(response);
+                } catch (ParserConfigurationException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (SAXException e) {
+                    e.printStackTrace();
+                } catch (TransformerException e) {
+                    e.printStackTrace();
+                }
             }
         }, new Response.ErrorListener()
         {
@@ -48,8 +81,41 @@ public class CURLActivity extends AppCompatActivity
                 mTextView.setText("That didn't work!");
             }
         }
-        );
+        )
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError
+            {
+                Map<String, String> headers = new HashMap<>();
+                String credentials = "dumcapp:JesusLORD0fa11!";
+                String auth = "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+                headers.put("Content-Type", "application/json");
+                headers.put("Authorization", auth);
+                return headers;
+            }
+        };
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
+    }
+
+    public void convertStringToDoc(String response) throws ParserConfigurationException, IOException, SAXException, TransformerException
+    {
+        DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = db.parse(new ByteArrayInputStream(response.getBytes("UTF-8"))); //remove the parameter UTF-8 if you don't want to specify the Encoding type.
+        printDocument(document, System.out);
+    }
+
+    public static void printDocument(Document doc, OutputStream out) throws IOException, TransformerException
+    {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+        transformer.transform(new DOMSource(doc),
+                new StreamResult(new OutputStreamWriter(out, "UTF-8")));
     }
 }
