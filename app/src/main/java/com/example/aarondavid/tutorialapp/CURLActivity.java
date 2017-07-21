ff0f000c3b139c0a1d0b33bc250281e50839f37f
codenameOXIDE <aaron.david91@gmail.com>
2017-07-21 12:17:58 +0800

    Updated Login Screen. Now logs in to CMS, Splash screen and main menu next

--- app/src/main/java/com/example/aarondavid/tutorialapp/CURLActivity.java
+++ app/src/main/java/com/example/aarondavid/tutorialapp/CURLActivity.java
@@ -1,37 +1,20 @@
 package com.example.aarondavid.tutorialapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
-import android.widget.TextView;
+import android.util.Log;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-
-import java.io.ByteArrayInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.Map;
+import java.net.URL;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+import javax.net.ssl.HttpsURLConnection;
 
 public class CURLActivity extends AppCompatActivity
 {
@@ -41,81 +24,76 @@ public class CURLActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_curl);
-        runRequestQueue();
-
+        RunHttpsRequest runHttp = new RunHttpsRequest();
+        runHttp.execute();
     }
 
-    public void runRequestQueue()
+    public void makeRequest() throws IOException
     {
-        final TextView mTextView = (TextView) findViewById(R.id.text);
+        Log.v("RESPONSE CODE", "Printing response code");
+        System.out.println(getXMLFromUrl("https://dumc.ccbchurch.com/api.php?srv=individual_id_from_login_password"));
+    }
 
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-
-        String url = "https://dumc.ccbchurch.com/api.php?srv=individual_profile_from_id&individual_id=9";
-
-        //Request a string from the provided url
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>()
+    public String getXMLFromUrl(String url)
+    {
+        BufferedReader br = null;
+        try
         {
-            @Override
-            public void onResponse(String response)
+            String auth = "dumcapp:JesusLORD0fa11!";
+            byte[] authEncByte = Base64.encode(auth.getBytes(), Base64.DEFAULT);
+            String authEncString = new String(authEncByte);
+
+            HttpsURLConnection connection = (HttpsURLConnection)(new URL(url)).openConnection();
+            connection.setRequestProperty("Authorization", "Basic " + authEncString);
+            connection.setRequestMethod("POST");
+
+            String params = "login=aaron.david@dumc.my&password=" + java.net.URLEncoder.encode("Nrtplay8", "UTF-8");
+            OutputStream os = connection.getOutputStream();
+            BufferedWriter writer = new BufferedWriter( new OutputStreamWriter(os, "UTF-8"));
+            writer.write(params);
+            writer.flush();
+            writer.close();
+            os.close();
+            br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String line;
+            final StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null)
             {
-                //mTextView.setText("Response is: " + response.substring(0, 6000));
-                try {
-                    convertStringToDoc(response);
-                } catch (ParserConfigurationException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (SAXException e) {
-                    e.printStackTrace();
-                } catch (TransformerException e) {
-                    e.printStackTrace();
-                }
+                sb.append(line).append("\n");
             }
-        }, new Response.ErrorListener()
+
+            return sb.toString();
+        }
+        catch (Exception e)
         {
-            @Override
-            public void onErrorResponse(VolleyError error)
+            e.printStackTrace();
+            return null;
+        }
+        finally
+        {
+            try
             {
-                mTextView.setText("That didn't work!");
+                if (br != null) br.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
             }
         }
-        )
+    }
+
+    private class RunHttpsRequest extends AsyncTask
+    {
+        @Override
+        protected Object doInBackground(Object[] params)
         {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError
-            {
-                Map<String, String> headers = new HashMap<>();
-                String credentials = "dumcapp:JesusLORD0fa11!";
-                String auth = "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
-                headers.put("Content-Type", "application/json");
-                headers.put("Authorization", auth);
-                return headers;
+            try {
+                makeRequest();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        };
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
-    }
-
-    public void convertStringToDoc(String response) throws ParserConfigurationException, IOException, SAXException, TransformerException
-    {
-        DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        Document document = db.parse(new ByteArrayInputStream(response.getBytes("UTF-8"))); //remove the parameter UTF-8 if you don't want to specify the Encoding type.
-        printDocument(document, System.out);
-    }
-
-    public static void printDocument(Document doc, OutputStream out) throws IOException, TransformerException
-    {
-        TransformerFactory tf = TransformerFactory.newInstance();
-        Transformer transformer = tf.newTransformer();
-        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-
-        transformer.transform(new DOMSource(doc),
-                new StreamResult(new OutputStreamWriter(out, "UTF-8")));
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/aarondavid/tutorialapp/LoginActivity.java
+++ app/src/main/java/com/example/aarondavid/tutorialapp/LoginActivity.java
@@ -1,15 +1,119 @@
 package com.example.aarondavid.tutorialapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener
 {
+    TextView username;
+    TextView password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.button);
+        username = (TextView) findViewById(R.id.usernameText);
+        password = (TextView) findViewById(R.id.passwordText);
+        loginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        String usernameString = username.getText().toString();
+        String passwordString = password.getText().toString();
+
+        //Toast.makeText(LoginActivity.this, "This is " + usernameString + " Using " + passwordString, Toast.LENGTH_SHORT).show();
+        RunHttpsRequest runHttpsRequest = new RunHttpsRequest();
+        runHttpsRequest.execute(usernameString, passwordString);
+    }
+
+    public void makeRequest(String un, String pass) throws IOException
+    {
+        Log.v("RESPONSE CODE", "Printing response code");
+        System.out.println(getXMLFromUrl("https://dumc.ccbchurch.com/api.php?srv=individual_id_from_login_password", un, pass));
+    }
+
+    public String getXMLFromUrl(String url, String userN, String passW)
+    {
+        BufferedReader br = null;
+        try
+        {
+            String auth = "dumcapp:JesusLORD0fa11!";
+            byte[] authEncByte = Base64.encode(auth.getBytes(), Base64.DEFAULT);
+            String authEncString = new String(authEncByte);
+
+            HttpsURLConnection connection = (HttpsURLConnection)(new URL(url)).openConnection();
+            connection.setRequestProperty("Authorization", "Basic " + authEncString);
+            connection.setRequestMethod("POST");
+
+            String params = "login="+userN+"&password=" + java.net.URLEncoder.encode(passW, "UTF-8");
+            OutputStream os = connection.getOutputStream();
+            BufferedWriter writer = new BufferedWriter( new OutputStreamWriter(os, "UTF-8"));
+            writer.write(params);
+            writer.flush();
+            writer.close();
+            os.close();
+            br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String line;
+            final StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null)
+            {
+                sb.append(line).append("\n");
+            }
+
+            return sb.toString();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+        finally
+        {
+            try
+            {
+                if (br != null) br.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class RunHttpsRequest extends AsyncTask
+    {
+        @Override
+        protected Object doInBackground(Object[] params)
+        {
+            try {
+                makeRequest(params[0].toString(), params[1].toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
